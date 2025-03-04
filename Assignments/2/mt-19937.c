d0f945c8d897159f50c5fa9425d10fd2e957e847
nikapadia <30702376+nikapadia@users.noreply.github.com>
2025-03-03 20:28:49 -0500

    assignment 2 done

--- Assignments/2/mt-19937.c
+++ Assignments/2/mt-19937.c
@@ -1,12 +1,12 @@
-/* 
+/*
    A C-program for MT19937, with initialization improved 2002/1/26.
    Coded by Takuji Nishimura and Makoto Matsumoto.
 
-   Before using, initialize the state by using init_genrand(seed)  
+   Before using, initialize the state by using init_genrand(seed)
    or init_by_array(init_key, key_length).
 
    Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.                          
+   All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
@@ -19,8 +19,8 @@
         notice, this list of conditions and the following disclaimer in the
         documentation and/or other materials provided with the distribution.
 
-     3. The names of its contributors may not be used to endorse or promote 
-        products derived from this software without specific prior written 
+     3. The names of its contributors may not be used to endorse or promote
+        products derived from this software without specific prior written
         permission.
 
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -42,159 +42,259 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <mpi.h>
+#include <math.h>
 
-/* Period parameters */  
+/* Period parameters */
 #define N 624
 #define M 397
 #define MATRIX_A 0x9908b0dfUL   /* constant vector a */
 #define UPPER_MASK 0x80000000UL /* most significant w-r bits */
 #define LOWER_MASK 0x7fffffffUL /* least significant r bits */
+#define BILLION_SAMPLES (1L << 30)
 
 static unsigned long mt[N]; /* the array for the state vector  */
-static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
+static int mti = N + 1;     /* mti==N+1 means mt[N] is not initialized */
 
 /* initializes mt[N] with a seed */
 void init_genrand(unsigned long s)
 {
-    mt[0]= s & 0xffffffffUL;
-    for (mti=1; mti<N; mti++) {
-        mt[mti] = 
-	    (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
+    mt[0] = s & 0xffffffffUL;
+    for (mti = 1; mti < N; mti++)
+    {
+        mt[mti] =
+            (1812433253UL * (mt[mti - 1] ^ (mt[mti - 1] >> 30)) + mti);
         /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
         /* In the previous versions, MSBs of the seed affect   */
         /* only MSBs of the array mt[].                        */
-		/* 2002/01/09 modified by Makoto Matsumoto             */
-		mt[mti] &= 0xffffffffUL;
-		/* for >32 bit machines */
-	    }
-	}
+        /* 2002/01/09 modified by Makoto Matsumoto             */
+        mt[mti] &= 0xffffffffUL;
+        /* for >32 bit machines */
+    }
+}
 
-	/* initialize by an array with array-length */
-	/* init_key is the array for initializing keys */
-	/* key_length is its length */
-	/* slight change for C++, 2004/2/26 */
-	void init_by_array(unsigned long init_key[], int key_length)
-	{
-	    int i, j, k;
-	    init_genrand(19650218UL);
-	    i=1; j=0;
-	    k = (N>key_length ? N : key_length);
-	    for (; k; k--) {
-		mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
-		  + init_key[j] + j; /* non linear */
-		mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
-		i++; j++;
-		if (i>=N) { mt[0] = mt[N-1]; i=1; }
-		if (j>=key_length) j=0;
-	    }
-	    for (k=N-1; k; k--) {
-		mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
-		  - i; /* non linear */
-		mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
-		i++;
-		if (i>=N) { mt[0] = mt[N-1]; i=1; }
-	    }
+/* initialize by an array with array-length */
+/* init_key is the array for initializing keys */
+/* key_length is its length */
+/* slight change for C++, 2004/2/26 */
+void init_by_array(unsigned long init_key[], int key_length)
+{
+    int i, j, k;
+    init_genrand(19650218UL);
+    i = 1;
+    j = 0;
+    k = (N > key_length ? N : key_length);
+    for (; k; k--)
+    {
+        mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1664525UL)) + init_key[j] + j; /* non linear */
+        mt[i] &= 0xffffffffUL;                                                             /* for WORDSIZE > 32 machines */
+        i++;
+        j++;
+        if (i >= N)
+        {
+            mt[0] = mt[N - 1];
+            i = 1;
+        }
+        if (j >= key_length)
+            j = 0;
+    }
+    for (k = N - 1; k; k--)
+    {
+        mt[i] = (mt[i] ^ ((mt[i - 1] ^ (mt[i - 1] >> 30)) * 1566083941UL)) - i; /* non linear */
+        mt[i] &= 0xffffffffUL;                                                  /* for WORDSIZE > 32 machines */
+        i++;
+        if (i >= N)
+        {
+            mt[0] = mt[N - 1];
+            i = 1;
+        }
+    }
 
-	    mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
-	}
+    mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */
+}
 
-	/* generates a random number on [0,0xffffffff]-interval */
-	unsigned long genrand_int32(void)
-	{
-	    unsigned long y;
-	    static unsigned long mag01[2]={0x0UL, MATRIX_A};
-	    /* mag01[x] = x * MATRIX_A  for x=0,1 */
+/* generates a random number on [0,0xffffffff]-interval */
+unsigned long genrand_int32(void)
+{
+    unsigned long y;
+    static unsigned long mag01[2] = {0x0UL, MATRIX_A};
+    /* mag01[x] = x * MATRIX_A  for x=0,1 */
 
-	    if (mti >= N) { /* generate N words at one time */
-		int kk;
+    if (mti >= N)
+    { /* generate N words at one time */
+        int kk;
 
-		if (mti == N+1)   /* if init_genrand() has not been called, */
-		    init_genrand(5489UL); /* a default initial seed is used */
+        if (mti == N + 1)         /* if init_genrand() has not been called, */
+            init_genrand(5489UL); /* a default initial seed is used */
 
-		for (kk=0;kk<N-M;kk++) {
-		    y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-		    mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
-		}
-		for (;kk<N-1;kk++) {
-		    y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-		    mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
-		}
-		y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-		mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        for (kk = 0; kk < N - M; kk++)
+        {
+            y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
+            mt[kk] = mt[kk + M] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        }
+        for (; kk < N - 1; kk++)
+        {
+            y = (mt[kk] & UPPER_MASK) | (mt[kk + 1] & LOWER_MASK);
+            mt[kk] = mt[kk + (M - N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
+        }
+        y = (mt[N - 1] & UPPER_MASK) | (mt[0] & LOWER_MASK);
+        mt[N - 1] = mt[M - 1] ^ (y >> 1) ^ mag01[y & 0x1UL];
 
-		mti = 0;
-	    }
-	  
-	    y = mt[mti++];
+        mti = 0;
+    }
 
-	    /* Tempering */
-	    y ^= (y >> 11);
-	    y ^= (y << 7) & 0x9d2c5680UL;
-	    y ^= (y << 15) & 0xefc60000UL;
-	    y ^= (y >> 18);
+    y = mt[mti++];
 
-	    return y;
-	}
+    /* Tempering */
+    y ^= (y >> 11);
+    y ^= (y << 7) & 0x9d2c5680UL;
+    y ^= (y << 15) & 0xefc60000UL;
+    y ^= (y >> 18);
 
-	/* generates a random number on [0,0x7fffffff]-interval */
-	long genrand_int31(void)
-	{
-	    return (long)(genrand_int32()>>1);
-	}
+    return y;
+}
 
-	/* generates a random number on [0,1]-real-interval */
-	double genrand_real1(void)
-	{
-	    return genrand_int32()*(1.0/4294967295.0); 
-	    /* divided by 2^32-1 */ 
-	}
+/* generates a random number on [0,0x7fffffff]-interval */
+long genrand_int31(void)
+{
+    return (long)(genrand_int32() >> 1);
+}
 
-	/* generates a random number on [0,1)-real-interval */
-	double genrand_real2(void)
-	{
-	    return genrand_int32()*(1.0/4294967296.0); 
-	    /* divided by 2^32 */
-	}
+/* generates a random number on [0,1]-real-interval */
+double genrand_real1(void)
+{
+    return genrand_int32() * (1.0 / 4294967295.0);
+    /* divided by 2^32-1 */
+}
 
-	/* generates a random number on (0,1)-real-interval */
-	double genrand_real3(void)
-	{
-	    return (((double)genrand_int32()) + 0.5)*(1.0/4294967296.0); 
-	    /* divided by 2^32 */
-	}
+/* generates a random number on [0,1)-real-interval */
+double genrand_real2(void)
+{
+    return genrand_int32() * (1.0 / 4294967296.0);
+    /* divided by 2^32 */
+}
 
-	/* generates a random number on [0,1) with 53-bit resolution*/
-	double genrand_res53(void) 
-	{ 
-	    unsigned long a=genrand_int32()>>5, b=genrand_int32()>>6; 
-	    return(a*67108864.0+b)*(1.0/9007199254740992.0); 
-	} 
-	/* These real versions are due to Isaku Wada, 2002/01/09 added */
+/* generates a random number on (0,1)-real-interval */
+double genrand_real3(void)
+{
+    return (((double)genrand_int32()) + 0.5) * (1.0 / 4294967296.0);
+    /* divided by 2^32 */
+}
 
-	unsigned long g_init[624];
-	unsigned long length=624;
+/* generates a random number on [0,1) with 53-bit resolution*/
+double genrand_res53(void)
+{
+    unsigned long a = genrand_int32() >> 5, b = genrand_int32() >> 6;
+    return (a * 67108864.0 + b) * (1.0 / 9007199254740992.0);
+}
+/* These real versions are due to Isaku Wada, 2002/01/09 added */
 
-	int main(void)
-	{
-	    int i;
-	    for( i = 0; i < length; i++ )
-	      {
-		g_init[i] = ((unsigned long)(1<<31) - 1) - (((unsigned long)(1<<21) - 1) * i) - (((unsigned long)(1<<15) - 1) * i); // add MPI rank adjustment
-		printf("Seed index %u == %lu (%lx)\n", i, g_init[i], g_init[i]);
-	      }
-	    
-	    init_by_array(g_init, length);
+unsigned long long calculate_pi(long long num_samples)
+{
+    unsigned long long local_hits = 0;
 
-	    printf("1000 outputs of genrand_int32()\n");
-	    for (i=0; i<1000; i++) {
-	      printf("%10lu ", genrand_int32());
-	      if (i%5==4) printf("\n");
-	    }
-	    printf("\n1000 outputs of genrand_real2()\n");
-	    for (i=0; i<1000; i++) {
-	      printf("%10.8f ", genrand_real2());
-	      if (i%5==4) printf("\n");
-	    }
-	    return 0;
-	}
+    for (unsigned long long i = 0; i < num_samples; i++)
+    {
+        double x = genrand_real1();
+        double y = genrand_real1();
+        if ((x * x + y * y) <= 1.0)
+        {
+            local_hits++;
+        }
+    }
 
+    return local_hits;
+}
+
+unsigned long long getticks(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return ((unsigned long long)tv.tv_sec * 1000000ULL) + tv.tv_usec;
+}
+
+int main(int argc, char *argv[])
+{
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // 0 for weak scaling, 1 for strong scaling
+    int scaling_type = 0;
+    if (argc > 1)
+    {
+        scaling_type = atoi(argv[1]);
+    }
+
+    unsigned long long num_samples;
+    if (scaling_type == 0)
+    {
+        num_samples = BILLION_SAMPLES;
+    }
+    else // strong scaling uses 16 billion samples
+    {
+        num_samples = (16 * BILLION_SAMPLES) / size;
+    }
+
+    unsigned long g_init[624];
+    unsigned long length = 624;
+    for (int i = 0; i < length; i++)
+    {
+        g_init[i] = ((1UL << 31) - 1) - (((1UL << 21) - 1) * i) - (((1UL << 15) - 1) * i) - (1023UL * rank);
+    }
+
+    init_by_array(g_init, length);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    unsigned long long start_time = getticks();
+    unsigned long long local_hits = 0;
+    local_hits = calculate_pi(num_samples);
+
+    unsigned long long global_hits = 0;
+    MPI_Reduce(&local_hits, &global_hits, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    unsigned long long end_time = getticks();
+    unsigned long long total_samples = num_samples * size;
+    double pi_estimate = 4.0 * ((double)global_hits / (double)total_samples);
+
+    if (rank == 0)
+    {
+        printf("Scaling type  : %d\n", scaling_type);
+        printf("MPI ranks     : %d\n", size);
+        printf("Samples/rank  : %llu\n", num_samples);
+        printf("Total samples : %llu\n", total_samples);
+        printf("Pi            : %.20f\n", pi_estimate);
+        printf("Time          : %llu\n", end_time - start_time);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
+/*
+Reference
+3.14159265358979323846
+
+Weak scaling
+Rank | Pi value
+1    | 3.14157021790742874146
+2    | 3.14156870357692241669
+4    | 3.14159007277339696884
+8    | 3.14159344043582677841
+16   | 3.14159532822668552399
+32   | 3.14159992977511137724
+64   | 3.14159421791555359960
+
+Strong scaling
+Rank | Pi value
+1    | 3.14157867897301912308
+2    | 3.14157873927615582943
+4    | 3.14159338269382715225
+8    | 3.14156896225176751614
+16   | 3.14159532822668552399
+32   | 3.14159107208251953125
+64   | 3.14157947618514299393
+*/
